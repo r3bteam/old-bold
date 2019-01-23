09af88512151225c3fe078629ec7c94730d7823f
r3bteam <39262475+r3bteam@users.noreply.github.com>
2019-01-23 19:24:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,136 +1,49 @@
+const Discord = require("discord.js");
 const {Client , RichEmbed} = require('discord.js');
 const client = new Client(); 
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
- client.on('message', msg => {
+  client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('<:look:533628235922276352> <a:idk:535523806685691905> pong');
   }
 });
-client.on('guildMemberRemove', (u) => {
-    u.guild.fetchAuditLogs().then( s => {
-        var ss = s.entries.first();
-        if (ss.action == "MEMBER_KICK") {
-        if (!data[ss.executor.id]) {
-            data[ss.executor.id] = { 
-            time : 1
-          };
-      } else { 
-          data[ss.executor.id].time+=1
-      }; 
-data[ss.executor.id].time = 0
-u.guild.roles.forEach(r => {
-	r.edit({
-                    permissions : 104160320
-                }); 
-                data[ss.executor.id].time = 0
-            });
-        setTimeout(function(){
-            if (data[ss.executor.id].time <= 3) { 
-                data[ss.executor.id].time = 0
-            }
-        },60000)
-    };
-    });
-    fs.writeFile("./data.json", JSON.stringify(data) ,(err) =>{
-        if (err) console.log(err.message);
-    });
-});
-client.on('roleDelete', (u) => { 
-    guild = u.guild;
-    roles = u.guild;
 
-    u.guild.fetchAuditLogs().then( s => {
-        var ss = s.entries.first();
-        if (ss.action == "ROLE_DELETE") {
-        if (!data[ss.executor.id]) {
-            data[ss.executor.id] = {
-            time : 1
-          };
-      } else {
-          data[ss.executor.id].time+=1
-      };
-data[ss.executor.id].time = 0
-u.guild.roles.forEach(r => {
-	r.edit({
-                    permissions : 104160320
-                }); 
-                data[ss.executor.id].time = 0
-            });
-        setTimeout(function(){
-            if (data[ss.executor.id].time <= 3) {
-                data[ss.executor.id].time = 0
-            }
-        },60000)
-    };
-    });
-    fs.writeFile("./data.json", JSON.stringify(data) ,(err) =>{
-        if (err) console.log(err.message);
-    });
+
+client.on('message', message => {
+  if(!message.channel.guild) return;
+var prefix = "-";
+if(message.content.startsWith(prefix + 'bc')) {
+if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+let copy = "Dragon";
+let request = `Requested By ${message.author.username}`;
+if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+msg.react('✅')
+.then(() => msg.react('❌'))
+.then(() =>msg.react('✅'))
+
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+
+reaction1.on("collect", r => {
+message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
+message.guild.members.forEach(m => {
+
+m.send(args)
+msg.delete();
+})
+})
+reaction2.on("collect", r => {
+message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+msg.delete();
+})
+})
+}
 });
-client.on('channelDelete', (u) => {
-    u.guild.fetchAuditLogs().then( s => { 
-        var ss = s.entries.first();
-        if (ss.action == "CHANNEL_DELETE") {
-        if (!data[ss.executor.id]) {
-            data[ss.executor.id] = {
-            time : 1
-          };
-      } else {
-          data[ss.executor.id].time+=1 
-      };
-data[ss.executor.id].time = 0
-roles.setPermissions(role => {
-    role.edit({
-    BAN_MEMBERS: false
-});                
-                data[ss.executor.id].time = 0
-            });
-        setTimeout(function(){
-            if (data[ss.executor.id].time <= 3) {
-                data[ss.executor.id].time = 0
-            }
-        },60000)
-    };
-    });
-    fs.writeFile("./data.json", JSON.stringify(data) ,(err) =>{
-        if (err) console.log(err.message);
-    });
-});
-client.on('guildBanAdd', (g , u) => {
-    g.fetchAuditLogs().then( s => {
-        var ss = s.entries.first();
-        if (ss.action == `MEMBER_BAN_ADD`) {
-        if (!data[ss.executor.id]) {
-            data[ss.executor.id] = {
-            time : 1
-          };
-      } else {
-          data[ss.executor.id].time+=1
-      };
-        if (data[ss.executor.id].time >= 3) {
-            g.roles.forEach(r => {
-                r.edit({ 
-                    permissions : 104160320
-                });  
-            }); 
-        }
-        setTimeout(function(){
-            if (data[ss.executor.id].time <= 3) {  
-                data[ss.executor.id].time = 0
-            } 
-        },60000)
-    }; 
-    });      
-    fs.writeFile("./data.json", JSON.stringify(data) ,(err) =>{    
-        if (err) console.log(err.message);   
-    });     
-});    
- client.on('guildMemberAdd', (member) => {
-member.addRole(member.guild.roles.find('name', 'Rois'));
-});
+
 client.login(process.env.BOT_TOKEN);
